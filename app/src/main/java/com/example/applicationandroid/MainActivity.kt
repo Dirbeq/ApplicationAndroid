14f86c2088cc08f62bb242feeb78dbfdc4696bfe
Dirbeq <dirbeq@gmail.com>
2022-01-01 20:26:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/applicationandroid/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.applicationandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button1:Button = findViewById(R.id.Zone_1)
+        val button2:Button = findViewById(R.id.Zone_2)
+
+    }
+}
